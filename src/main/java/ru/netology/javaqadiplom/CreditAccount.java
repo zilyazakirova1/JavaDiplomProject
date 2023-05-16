31f6577fa6d64e4f02918c8c28384a050a50c630
Neto test <zilya.zakirova1991@gmail.com>
2023-05-16 23:18:55 +0300

    Revert "my changhes"
    This reverts commit 5d4cbbced66cac959151831cea7acd18359935a1.

--- src/main/java/ru/netology/javaqadiplom/CreditAccount.java
+++ src/main/java/ru/netology/javaqadiplom/CreditAccount.java
@@ -13,10 +13,9 @@ public class CreditAccount extends Account {
      * Создаёт новый объект кредитного счёта с заданными параметрами.
      * Если параметры некорректны (кредитный лимит отрицательный и так далее), то
      * должно выкидываться исключения вида IllegalArgumentException.
-     *
      * @param initialBalance - неотрицательное число, начальный баланс для счёта
-     * @param creditLimit    - неотрицательное число, максимальная сумма которую можно задолжать банку
-     * @param rate           - неотрицательное число, ставка кредитования для расчёта долга за отрицательный баланс
+     * @param creditLimit - неотрицательное число, максимальная сумма которую можно задолжать банку
+     * @param rate - неотрицательное число, ставка кредитования для расчёта долга за отрицательный баланс
      */
     public CreditAccount(int initialBalance, int creditLimit, int rate) {
         if (rate <= 0) {
@@ -28,33 +27,13 @@ public class CreditAccount extends Account {
         this.creditLimit = creditLimit;
         this.rate = rate;
     }
-/*
-    public CreditAccount(int initialBalance, int creditLimit, int rate) throws IllegalArgumentException {
-        if (rate <= 0) {
-            throw new IllegalArgumentException(
-                    "Накопительная ставка не может быть отрицательной, а у вас: " + rate
-            );
-        } else if (initialBalance < 0) {
-            throw new IllegalArgumentException(
-                    "Начальный баланс для счёта не может быть отрицательным, а у вас: " + initialBalance
-            );
-        } else if (creditLimit<0){
-            throw new IllegalArgumentException(
-                    "Максимальная сумма которую можно задолжать банку не может быть отрицательной, а у вас: " +creditLimit
-            );
-        }
-        this.balance = initialBalance;
-        this.creditLimit = creditLimit;
-        this.rate = rate;
-    }
-*/
+
     /**
      * Операция оплаты с карты на указанную сумму.
      * В результате успешного вызова этого метода, баланс должен уменьшиться
      * на сумму покупки. Если же операция может привести к некорректному
      * состоянию счёта (например, баланс может уйти в минус), то операция должна
      * завершиться вернув false и ничего не поменяв на счёте.
-     *
      * @param amount - сумма покупки
      * @return true если операция прошла успешно, false иначе.
      */
@@ -72,16 +51,15 @@ public class CreditAccount extends Account {
         }
     }
 
-     /**
+    /**
      * Операция пополнения карты на указанную сумму.
      * В результате успешного вызова этого метода, баланс должен увеличиться
      * на сумму покупки. Если же операция может привести к некорректному
      * состоянию счёта, то операция должна
      * завершиться вернув false и ничего не поменяв на счёте.
-     *
      * @param amount - сумма пополнения
-     * @param amount
      * @return true если операция прошла успешно, false иначе.
+     * @param amount
      * @return
      */
     @Override
@@ -93,41 +71,21 @@ public class CreditAccount extends Account {
         return true;
     }
 
-  /*  @Override
-    public boolean add(int amount) {
-        if (amount <= 0) {
-            return false;
-        }else{
-        balance = balance+amount;
-        return true;
-        }
-    }
-    */
-
-
     /**
      * Операция расчёта процентов на отрицательный баланс счёта при условии, что
      * счёт не будет меняться год. Сумма процентов приводится к целому
      * числу через отбрасывание дробной части (так и работает целочисленное деление).
      * Пример: если на счёте -200 рублей, то при ставке 15% ответ должен быть -30.
      * Пример 2: если на счёте 200 рублей, то при любой ставке ответ должен быть 0.
-     *
      * @return
      */
     @Override
-    public int yearChange() {return balance / 100 * rate;}
-
-  /*  @Override
     public int yearChange() {
-        if (balance >= 0) {
-            return 0;
-        } else {
-            return balance / 100 * rate;
-        }
+        return balance / 100 * rate;
     }
-*/
 
     public int getCreditLimit() {
         return creditLimit;
     }
 }
+//Hi
