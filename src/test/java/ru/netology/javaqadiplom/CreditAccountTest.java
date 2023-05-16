5d4cbbced66cac959151831cea7acd18359935a1
Neto test <zilya.zakirova1991@gmail.com>
2023-05-16 23:13:36 +0300

    my changhes

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -9,12 +9,88 @@ public class CreditAccountTest {
     public void shouldAddToPositiveBalance() {
         CreditAccount account = new CreditAccount(
                 0,
-                5_000,
+                5000,
                 15
         );
 
-        account.add(3_000);
+        account.add(3000);
 
-        Assertions.assertEquals(3_000, account.getBalance());
+        Assertions.assertEquals(3000, account.getBalance());
+    }
+    @Test
+    public void shouldAddToNegativeBalance() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5000,
+                15
+        );
+
+        account.add(-3000);
+
+        Assertions.assertEquals(0, account.getBalance());
+    }
+
+    @Test
+    public void shouldAddToZeroBalance() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5000,
+                15
+        );
+
+        account.add(0);
+
+        Assertions.assertEquals(0, account.getBalance());
+    }
+
+    @Test
+    public void shouldToNegativeInitialBalance() {
+        Assertions.assertThrows(IllegalArgumentException.class,() -> new CreditAccount(-2000,5000,15));
+    }
+    @Test
+    public void shouldToNegativeCreditLimit() {
+        Assertions.assertThrows(IllegalArgumentException.class,() -> new CreditAccount(0,-5000,15));
+    }
+    @Test
+    public void shouldToNegativeRate() {
+        Assertions.assertThrows(IllegalArgumentException.class,() -> new CreditAccount(0,5000,-15));
+    }
+
+    @Test
+    public void shouldPayToPositiveAmount() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5000,
+                15
+        );
+
+        account.pay(3000);
+
+        Assertions.assertEquals(-3000, account.getBalance());
+    }
+    @Test
+    public void shouldPayToNegativeAmount() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5000,
+                15
+        );
+
+        account.pay(-3000);
+
+        Assertions.assertEquals(0, account.getBalance());
+    }
+
+    @Test
+    public void shouldPayToZeroAmount() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5000,
+                15
+        );
+
+        account.pay(0);
+
+        Assertions.assertEquals(0, account.getBalance());
     }
 }
